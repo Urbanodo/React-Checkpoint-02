d8e336072a21a3db6c43e3ec2c46cc3f15228a0d
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2025-12-31 13:48:24 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import PlayersList from "./PlayersList";
 function App() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Liste des joueurs ⚽ </h1>
+      <h1 className="text-primary" style={{ textAlign: "center" }}>Voici mes joueurs préférés </h1>
+      <h2 style={{ textAlign: "center" }}>Liste des joueurs ⚽ </h2>
       <PlayersList />
     </div>
   );
