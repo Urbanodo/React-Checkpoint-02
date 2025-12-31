bb20abaa9c2f30e9eaf25d9fa4c1e4298fc8d1b0
Urbain Yaovi DOHOUNGBO <urbanodohoungbo@gmail.com>
2025-12-31 12:29:15 +0000

    first commit

--- src/players.js
+++ src/players.js
@@ -30,6 +30,54 @@ const players = [
     numero: 10,
     age: 32,
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJevwU5hcJnkUkgPNZa0i2CYUaeXg_PJpWPw&s"
+  },
+  {
+    nom: "Mattéo AHLINVI",
+    equipe: "Arsenal tula",
+    nationalite: "Béninoise",
+    numero: 7,
+    age: 26,
+    image: "https://www.footballdatabase.eu/images/photos/players/a_321/321145.jpg"
+  },
+  {
+    nom: "Mohamed Tidjani",
+    equipe: "Yverdon-Sport FC",
+    nationalite: "Béninoise",
+    numero: 13,
+    age: 28,
+    image: "https://srtb.bj/storage/2022/05/Mohamed-Tidjani.png"
+  },
+  {
+    nom: "Lamine YAMAL",
+    equipe: "FC Barcelone",
+    nationalite: "Espagnole",
+    numero: 10,
+    age: 18,
+    image: "https://www.fcbarcelona.com/photo-resources/2025/09/09/a9ecee2c-116c-405c-8524-3127913e7a3c/10-Lamine.jpg?width=1200&height=750"
+  },
+  {
+    nom: "Alejandro Balde",
+    equipe: "FC Barcelone",
+    nationalite: "Espagnole",
+    numero: 3,
+    age: 22,
+    image: "https://www.fcbarcelona.com/photo-resources/2025/09/09/82dfc9f5-ffc4-4b47-a828-21de924f9b5f/03-Balde.jpg?width=1200&height=750"
+  },
+   {
+    nom: "Lionel MESSI",
+    equipe: "FC Inter-MIAMI",
+    nationalite: "Argentin",
+    numero: 10,
+    age: 38,
+    image: "https://www.africatopsuccess.com/wp-content/uploads/2025/12/Leo-Messi-660x330.webp"
+  },
+  {
+    nom: "Raphael Dias Belloli",
+    equipe: "FC Barcelone",
+    nationalite: "Bresilienne",
+    numero: 11,
+    age: 29,
+    image: "https://www.onzedafrik.com/wp-content/uploads/2025/04/WhatsApp-Image-2025-04-26-at-6.57.31-PM-810x580.jpeg"
   }
 ];
 
